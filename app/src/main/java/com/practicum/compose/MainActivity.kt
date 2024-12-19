9655d047bd69ecf6797649229178e41e6d069c38
PC <anikin13@gmail.com>
2024-12-19 20:43:21 +0400

    Simple Immutable LazyColumn

--- app/src/main/java/com/practicum/compose/MainActivity.kt
+++ app/src/main/java/com/practicum/compose/MainActivity.kt
@@ -3,36 +3,71 @@ package com.practicum.compose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.ScrollState
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.verticalScroll
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Photo
+import androidx.compose.material3.Card
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment.Companion.CenterVertically
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         //enableEdgeToEdge()
         setContent {
-            SimpleColumnScreen()
+            SimpleLazyColumn(List(1000) { it.toString() })
         }
     }
 }
 
 @Composable
-fun SimpleColumnScreen() {
-    Column(
+fun SimpleLazyColumn(customItems: List<String>) {
+    LazyColumn(modifier = Modifier.fillMaxSize()) {
+        items(customItems) { number ->
+            PhotoView(number)
+        }
+    }
+}
+
+@Composable
+fun PhotoView(name: String) {
+    Card(
         modifier = Modifier
-            .fillMaxSize()
-            .verticalScroll(ScrollState(0))
-    ) {
-        repeat(1000) { value ->
-            Text(text = value.toString(), modifier = Modifier.fillMaxWidth(), textAlign = TextAlign.Center)
+            .fillMaxWidth()
+            .padding(8.dp)
+    )
+    {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(6.dp)
+        ) {
+            Icon(
+                imageVector = Icons.Default.Photo,
+                contentDescription = "Person",
+                modifier = Modifier
+                    .size(60.dp)
+            )
+            Text(
+                text = name,
+                modifier = Modifier.align(CenterVertically),
+                style = MaterialTheme.typography.headlineLarge.copy(
+                    fontWeight = FontWeight.Bold
+                )
+            )
         }
     }
 }
@@ -40,5 +75,5 @@ fun SimpleColumnScreen() {
 @Preview(showBackground = true, widthDp = 320)
 @Composable
 fun SimpleColumnScreenPreview() {
-    SimpleColumnScreen()
+    SimpleLazyColumn(List(1000) { it.toString() })
 }
